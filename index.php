7e0c3a26fd016beea5a51120c4b307b9ec2504f7
Binny V A <binnyva@gmail.com>
2014-05-20 19:08:59 +0530

    Fixed an issue when I added an extra level. Now the last level won't show in the to contact list

--- index.php
+++ index.php
@@ -20,11 +20,12 @@ $all_levels = keyFormat($t_level->get('byid'), array('id','name'));
 // This will show all the people in various levels who have been uncontacted for X days or more.
 $contact_thresholds = $sql->getById("SELECT element_id,`interval` FROM Frequency 
 				WHERE user_id='$_SESSION[user_id]' AND data_type='level' AND type='any'");
+$last_level_id = $sql->getOne("SELECT MAX(id) FROM Level");
 // Get all people in all levels execpt 4(Aquancences).
 $people_last_contact = $sql->getAll("SELECT P.id,P.nickname,P.name,P.level_id, MAX(C.start_on) AS last_contact_on FROM Person P 
 			INNER JOIN PersonConnection PC ON P.id=PC.person_id 
 			INNER JOIN Connection C ON PC.connection_id=C.id 
-			WHERE P.level_id!=4 AND P.user_id=$_SESSION[user_id]
+			WHERE P.level_id!=$last_level_id AND P.user_id=$_SESSION[user_id]
 				GROUP BY PC.person_id ORDER BY P.level_id");
 
 $uncontacted_people = array();
